647abfb82fa1d33447c2d3452334ac7058324bde
Viktor Stenby <s174483@student.dtu.dk>
2020-09-22 15:49:13 +0200

    Updated opt_alpha such that it has a waitbar

--- functions/opt_alpha.m
+++ functions/opt_alpha.m
@@ -7,9 +7,11 @@ end
 
 error_dist = zeros(length(alphavec),1);
 xalpha = zeros(numel(x),length(alphavec));
-
+f = waitbar(0, 'Evaluating alphas');
 for i=1:length(alphavec)
    xalpha(:,i) = mosek_TikhNN(A,b,alphavec(i)).*factor;
    error_dist(i) = norm(xalpha(:,i) - x(:),2);
+   waitbar(i/length(alphavec),f,'Evaluating alphas')
 end
+close(f)
 end
