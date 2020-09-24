d2f9267cd9c470432a7e751e98d39a0d6e31f7b5
Viktor Stenby <s174483@student.dtu.dk>
2020-09-24 22:50:23 +0200

    Small changes

--- testprobs/biMax_HPC.m
+++ testprobs/biMax_HPC.m
@@ -1,10 +1,25 @@
 %% Program to run on HPC computer.
-clear, clc, close all
+%clear, clc, close all
 
-idx = 1;
 while 1
-    run('biMax_UQ')
-    dataname = strcat('test',num2str(idx),'.mat');
-    save(dataname,'lamsamp','delsamp','xsamp')
-    idx = idx + 1;
+    run('biMax_UQ') 
+    n = find_newest_name();
+    dataname = strcat('sim',num2str(n),'.mat');
+    save(dataname,'A','S','x_sim','del_sim','lam_sim','alph_sim')
+end
+
+function n = find_newest_name()
+%Finds the next sim number in the folder.
+sims = {dir('sim*.mat').name};
+n = 0;
+for i=1:length(sims)
+    temp_str = sims{i};
+    temp_str = strrep(temp_str,'sim','');
+    temp_str = strrep(temp_str,'.mat','');
+    num = str2double(temp_str);
+    if num > n
+        n = num;
+    end
+end
+n = n+1;
 end
