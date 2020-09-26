0ba1e8c09b8779d4fb274fdfadf6e9dbbcaa3b26
Viktor Stenby Johansson <s174483@student.dtu.dk>
2020-09-26 22:40:27 +0200

    Slight changes

---
+++ simulation/simulations.m
@@ -0,0 +1,8 @@
+%% In this MATLAB script, the different simulation folders can be looked at.
+clear, clc, close all
+
+%Add dependencies.
+addpath(genpath('../functions'))
+addpath(genpath('../../aux'))
+
+analyze_sim('biMax_UQ')
