1d8e241358cd8fd10ab77b01db1c2df767619f5b
Viktor Stenby <s174483@student.dtu.dk>
2020-09-30 18:26:11 +0200

    Renamed biMax test - still looking for a better name.

--- testprobs/biMax_AnalyticTestCase.m
+++ testprobs/biMax_AnalyticTestCase.m
@@ -1,36 +1,45 @@
 %% Try to recreate the "AnalyticTestCase" with my own code.
 clear, clc, close all
 
-nsim = 250;
+nsim = 1000;
 
 %Here, we use some other values for some reason than the default.
-nvpara = 40;
-nvperp = 20;
-[vpara, vperp, gridinfo] = construct_vgrid(-4e6,4e6,nvpara,1e4,4e6,nvperp);
-options.Mi = 2*1.6726e-27; 
-[x_true, xinfo] = biMaxF(vpara,vperp,20*1000,20*1000,5e5,options);
+vparamin = -4e6;
+vparamax = 4e6;
+vparadim = 40;
 
-%phi = [10 20 40 70 85];
-phi = [10 20 40];
-u   = [-5:0.1:5]*1e6;
+vperpmin = 1e4;
+vperpmax = 4e6;
+vperpdim = 20;
 
-[b, binfo] = biMaxS(u,phi,20*1000,20*1000,5e5,options);    
-A = biMaxW(1,xinfo,binfo);
+Tpara = 2e4;
+Tperp = 2e4;
+vparadrift = 5e5;
 
-[b_noisy, ~, e] = add_noise(b);
+options.Mi = 2*1.6726e-27;
+
+phi = [10 20 40 70 85];
+u   = [-5e6:1e5:5e6];
+ubroadening = 1;
+
+%Alphas
+nalpha = 10;
+alpha = logspace(-10,-5,nalpha);
+
+[vpara, vperp, gridinfo] = construct_vgrid(vparamin,vparamax,vparadim,vperpmin,vperpmax,vperpdim);
+[x_true, xinfo] = biMaxx(vpara,vperp,Tpara,Tperp,vparadrift,options);
+[b, binfo] = biMaxb(u,phi,Tpara,Tperp,vparadrift,options);    
+A = biMaxA(ubroadening,xinfo,binfo);
+
+[b_noisy, ~, e] = add_noise(b); %Add noise to b.
 
 %The normalization used in AnalyticTestCase.m
 [A_norm, b_norm] = error_normalization(A, b_noisy, e);
 
 %Per Christians way of constructing L.
-L = reguL(nvpara,nvperp);
+L = reguL(vparadim,vperpdim);
 
-nalpha = 10;
-alpha = logspace(-10,-8,nalpha);
-
-tic
-[xalpha, resnorm, residual, exitflag, output] = mosek_TikhNN(A_norm,b_norm,alpha);
-toc
+[xalpha, resnorm, residual, exitflag, output] = mosek_TikhNN(A_norm,b_norm,alpha,L);
 
 % Relative error for each of these alphas.
 r = relerr(x_true(:),xalpha);
@@ -46,13 +55,6 @@ plot(alphaopt,ropt,'k.','MarkerSize',15)
 %Analyze the simulations.
 [x_mu, x_std, p] = analyze_sim(nsim, x_sim, alph_sim, del_sim, lam_sim, gridinfo);
 
-
 % Relative error
 r1 = relerr(x_true(:),x_mu); %Mean of posterior distribution.
 r2 = relerr(x_true(:),xalpha(:,idx)); %Optimal xalpha
-
-figure
-subplot(1,2,1)
-showDistribution(x_mu,gridinfo); title(['Posterior mean, relerr=', num2str(r1)])
-subplot(1,2,2)
-showDistribution(xalpha(:,idx),gridinfo); title(['optimal xalpha, relerr=', num2str(r2)])
