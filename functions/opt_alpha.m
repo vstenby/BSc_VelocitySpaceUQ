6b03f4e2132bd34c30503086b2a6f98dd64e8984
Viktor Stenby <s174483@student.dtu.dk>
2020-09-30 16:59:40 +0200

    Rewrote opt_alpha function. Still need rehaul because mosek_TikhNN now takes alpha as a vector.

--- functions/opt_alpha.m
+++ functions/opt_alpha.m
@@ -1,15 +1,15 @@
-function [xalpha, error_dist] = opt_alpha(alphavec, A, b, x, factor)
+function [xalpha, error_dist] = opt_alpha(alphavec, A, b, x, Lsq)
 %Find the optimal solution given a vector of alpha values.
 
 if nargin < 5
-    factor = 1;
+    Lsq = eye(numel(x));
 end
 
 error_dist = zeros(length(alphavec),1);
 xalpha = zeros(numel(x),length(alphavec));
 f = waitbar(0, 'Evaluating alphas');
 for i=1:length(alphavec)
-   xalpha(:,i) = mosek_TikhNN(A,b,alphavec(i)).*factor;
+   xalpha(:,i) = mosek_TikhNN(A,b,alphavec(i),Lsq);
    error_dist(i) = norm(xalpha(:,i) - x(:),2);
    waitbar(i/length(alphavec),f,'Evaluating alphas')
 end
