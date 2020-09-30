5021ffc001ac8a723225db2dd8c958bf7d1c9ef6
Viktor Stenby <s174483@student.dtu.dk>
2020-09-30 17:57:27 +0200

    Update mosek_TikhNN.m

--- functions/mosek_TikhNN.m
+++ functions/mosek_TikhNN.m
@@ -10,28 +10,28 @@ function [x, resnorm, residual, exitflag, output] = mosek_TikhNN(A, b, alphavec,
 output.algorithm = 'MOSEK';
 output.iterations = zeros(length(alphavec),1);
 
+if nargin == 3
+    L = speye(n);
+elseif (nargin ~= 3 && nargin~=4)
+    error('Wrong number of inputs')
+end
+
 %TODO: Rewrite allocation of output elements to speed up the for loop.
 for i=1:length(alphavec) %If alpha is a vector.
     alpha = alphavec(i);
-    if nargin == 3
-        %0th order Tikhonov.
-        C = [A ; sqrt(alpha) * speye(n)];
-        d = [b ; zeros(n,1)];
-    elseif nargin == 4
-        %Mirko's original code.
-        %C = [A ; sqrt(alpha) * L1vpara ; sqrt(alpha) * L1vperp ; 2e-6*sqrt(alpha)*speye(n)]; %why 2e-6?
-        %d = [b ; zeros(2*size(L1vpara,2),1) ; zeros(n,1)];
-        
-        %My rewritten code to work with L1vperp and L1vpara.
-        %C = [A ; sqrt(alpha) * L1vpara ; sqrt(alpha) * L1vperp]; 
-        %d = [b ; zeros(2*size(L1vpara,2),1)]
-        
-        %Rewritten code to work with Per Christian's L.
-        C = [A ; sqrt(alpha)*L];
-        d = [b ; zeros(n,1)];
-    else
-        error('Wrong number of inputs')
-    end
+    
+    %Mirko's original code.
+    %C = [A ; sqrt(alpha) * L1vpara ; sqrt(alpha) * L1vperp ; 2e-6*sqrt(alpha)*speye(n)]; %why 2e-6?
+    %d = [b ; zeros(2*size(L1vpara,2),1) ; zeros(n,1)];
+
+    %My rewritten code to work with L1vperp and L1vpara.
+    %C = [A ; sqrt(alpha) * L1vpara ; sqrt(alpha) * L1vperp]; 
+    %d = [b ; zeros(2*size(L1vpara,2),1)]
+
+    %Rewritten code to work with Per Christian's L.
+    C = [A ; sqrt(alpha)*L];
+    d = [b ; zeros(n,1)];
+   
     [x(:,i), resnorm(i), residual(:,i), exitflag(i), output_temp] = lsqnonneg(C, d);
     if ~strcmpi(output_temp.algorithm,'MOSEK'), error('MOSEK is not used'), end
     output.iterations(i) = output_temp.iterations;
