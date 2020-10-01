9bd560e1cce8b746f16f04bdcb068d0fab6e7007
Viktor Stenby <s174483@student.dtu.dk>
2020-10-01 12:13:26 +0200

    Updated NNHGS_UQ such that it works with L.

--- functions/NNHGS_UQ.m
+++ functions/NNHGS_UQ.m
@@ -1,19 +1,15 @@
 function [x_sim, del_sim, lam_sim, alph_sim] = NNHGS_UQ(A, b, alpha0, L, n, disp_waitbar)
 %Nonnegative Hierachical Gibbs Sampler.
-%L = Lsq'*Lsq
 
 if nargin <= 5
     disp_waitbar = 0;  
 end
 
-%Compute the factorization such that Lsq' * Lsq = L;
-Lsq = chol(L); 
+[M,N] = size(A);
 
 if disp_waitbar
     f = waitbar(1/n,'Finding initial xalpha');
 end
-
-[M,N] = size(A);
 
 %Allocate vectors for sampling.
 del_sim    = zeros(n,1);
@@ -23,6 +19,8 @@ x_sim      = zeros(N,n);
 
 %Find the initial xalpha
 xtemp = mosek_TikhNN(A,b,alpha0,L);
+
+LtL = L'*L;
 
 %Set the initial values for alpha, x, delta and lambda.
 alph_sim(1)    = alpha0;
@@ -36,31 +34,30 @@ t0         = 0.0001;
 a1         = 1; 
 t1         = 0.0001;
 
-%Calculate this beforehand.
-Atb = A'*b;
-
 for i=2:n
    if disp_waitbar, waitbar(i/n, f, 'Sampling...'), end
    Axtemp = A*xtemp;
-   Lxtemp = L*xtemp;
+   LtLxtemp = LtL*xtemp;
    
    %Note here that 1./ is because of the way MATLAB does gamrnd. 
-   %Equation (58) and (59) in Dental CT.
+   %Equation (58) and (59) in Dental CT
    lam_sim(i)  = gamrnd(a0 + M/2, 1./(t0+norm(Axtemp(:)-b(:))^2/2)); 
-   del_sim(i)  = gamrnd(a1 + N/2, 1./(t1+xtemp(:)'*Lxtemp(:)/2));   
+   del_sim(i)  = gamrnd(a1 + N/2, 1./(t1+xtemp(:)'*LtLxtemp(:)/2)); 
    
    alph_sim(i) = del_sim(i)/lam_sim(i);
    
-   %2nd term of RHS, (69) in Dental CT.
-   eta = sqrt(lam_sim(i))*A'*randn(M,1) + sqrt(del_sim(i)).*Lsq'*randn(N,1);
+   %Generate bhat and chat.
+   bhat = mvnrnd(b,lam_sim(i)^(-1)*speye(M))';
+   chat = mvnrnd(zeros(N,1),del_sim(i)^(-1)*speye(N))';
    
-   %RHS is divided with lambda.
-   c = Atb + eta/lam_sim(i);
+   %Right hand side of (2.5) in BaHa20 divided with lambda.
+   rhs = A'*bhat + alph_sim(i)*L*chat;
    
-   %LHS is divided with lambda as well such that we get alpha instead.
-   B = @(x) (A' * (A*x)) + alph_sim(i)*L*x;
+   %LHS is divided with lambda.
+   B = @(x) (A' * (A*x)) + alph_sim(i)*LtL*x;
    
-   xtemp = GPCG(B, c, zeros(N,1), 50, 5, 20, 1e-6);
+   xtemp = GPCG(B, rhs, zeros(N,1), 50, 5, 20, 1e-6);
+   %xtemp = GPCG(B, rhs, zeros(N,1));
    
    x_sim(:,i) = xtemp;
     
