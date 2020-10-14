22377ee2dc0fea9c7b54722252299f5a0134db01
Viktor Stenby <s174483@student.dtu.dk>
2020-10-14 15:12:05 +0200

    Added trim and burnin for Geweke test.

--- functions/geweketest.m
+++ functions/geweketest.m
@@ -1,4 +1,4 @@
-function [p, pall] = geweketest(v)
+function [p, pall] = geweketest(v, burnin, trim)
 %   Returns the p test statistic from the Geweke test.
 %
 %   geweketest(v)
@@ -6,6 +6,20 @@ function [p, pall] = geweketest(v)
 %   geweke test is run on each column and the largest test statistic
 %   is returned.
 %
+
+if nargin == 1
+   burnin = 0;
+end
+
+if nargin == 2
+   %Remove burnin from v
+   v = v(burnin+1:end,:);
+end
+
+if nargin == 3
+   v = v(burnin+1:end,:);
+   v = v(1:trim:end, :); 
+end
 
 T = size(v,2);
 pall = zeros(T,1);
