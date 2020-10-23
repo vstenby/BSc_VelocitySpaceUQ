7b63e42ac9febd7bf10ef35137e042e9cc032e3a
Viktor Stenby <s174483@student.dtu.dk>
2020-10-23 12:21:37 +0200

    Missed a semicolon

--- functions/showDistribution.m
+++ functions/showDistribution.m
@@ -8,7 +8,7 @@ if nargin <= 2
     caxis_limits = [min(X(:)), max(X(:))];
 end
 imagesc(gridinfo.vpara_ax, gridinfo.vperp_ax, X); axis xy; axis image;
-caxis(caxis_limits)
+caxis(caxis_limits);
 xlabel('v_{||}','FontSize',10)
 ylabel('v_{\perp}','FontSize',10)
 set(gca, 'TickLabelInterpreter', 'latex')
