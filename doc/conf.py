15941463255cfed35c784881b1f04e0ce268fc12
Viktor Stenby <s174483@student.dtu.dk>
2020-10-29 14:54:25 +0100

    Minor changes to conf

--- doc/conf.py
+++ doc/conf.py
@@ -35,7 +35,7 @@ release = '0.1'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.mathjax','sphinx.ext.autodoc','sphinxcontrib.matlab']
+extensions = ['sphinx.ext.mathjax','sphinx.ext.autodoc','sphinxcontrib.matlab','sphinx.ext.extlinks']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -52,6 +52,8 @@ exclude_patterns = []
 # a list of builtin themes.
 #
 html_theme = 'sphinx_rtd_theme'
+html_show_sourcelink = False
+
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
